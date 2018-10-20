4d295e51e84c5611e07cb00371e0764a64080c00
ndry93 <ndry93@192.168.0.18>
2018-10-20 14:50:12 +0700

    init

---
+++ src/main/java/com/hy/gaeaadmin/GaeaAdminApplication.java
@@ -0,0 +1,12 @@
+package com.hy.gaeaadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaeaAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaeaAdminApplication.class, args);
+	}
+}
